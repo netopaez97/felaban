9ed8ce10c1e8513fb1b0e2dda1f13b3c5851598d
Neto <netopaez97@hotmail.com>
2019-10-18 00:59:45 -0500

    Mejoras en home.dart

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,11 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.grey,
+        primarySwatch: Colors.blue,
+        primaryColor: Colors.grey
       ),
       home: HomePage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -10,19 +10,24 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
 
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   AppBar _barraSuperior(){
     return AppBar();
   }
 
   Widget _cuerpoDeLaVista(){
     return ListView(
-        children: <Widget>[
-          _stackSuperior(),
-          _accesoConHuellaDigitalOReconocimientoFacial(),
-          _usuarioContrasena(),
-          _botonNext(),
-          _nuevoUsuarioOlvidoPass(),
-        ],
+      shrinkWrap: true,
+      children: <Widget>[
+        _stackSuperior(),
+        _textoAccesoConHuellaDigitalOReconocimientoFacial(),
+        _imagenAccesoConHuellaDigitalOReconocimientoFacial(),
+        Container(height: MediaQuery.of(context).size.width*0.02),
+        _botonCorreoElectronico(),
+        _botonContrasena(),
+        _botonNext(),
+      ],
     );
   }
 
@@ -31,101 +36,203 @@ class _HomePageState extends State<HomePage> {
       height: MediaQuery.of(context).size.height*0.3,
       child: Stack(
         children: <Widget>[
-          Image.asset("assets/logo.JPG")
+          Container(
+            width: double.infinity,
+            child: Image.asset(
+              "assets/background.png",
+              fit: BoxFit.fitWidth,
+              color: Colors.black54,
+              colorBlendMode: BlendMode.colorDodge,
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(vertical: MediaQuery.of(context).size.width*0.05),
+            alignment: Alignment.bottomCenter,
+            width: double.infinity,
+            child: Text(
+              "Welcome to the FELABAN APP",
+              style: TextStyle(color: Colors.white),
+            )
+          ),
+          Container(
+            padding: EdgeInsets.all(MediaQuery.of(context).size.width*0.03),
+            alignment: Alignment.topCenter,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Expanded(
+                    child: Image.asset(
+                    "assets/felabanLogo.png"
+                  ),
+                ),
+                Expanded(
+                    child: Image.asset(
+                    "assets/IFCLogo.png",
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
   }
 
-  Widget _accesoConHuellaDigitalOReconocimientoFacial(){
+  Widget _textoAccesoConHuellaDigitalOReconocimientoFacial(){
     return Container(
-      padding: EdgeInsets.all(MediaQuery.of(context).size.width*0.03), 
-      child: Column(
+      padding: EdgeInsets.symmetric(horizontal:  MediaQuery.of(context).size.width*0.03), 
+      height: MediaQuery.of(context).size.height*0.1,
+      alignment: Alignment.center,
+      child: Text(
+        "Log in to access your networking profile and customize the app for your event experience",
+        textAlign: TextAlign.center,
+      ),
+    );
+  }
+
+  Widget _imagenAccesoConHuellaDigitalOReconocimientoFacial(){
+    return Container(
+      height: MediaQuery.of(context).size.height*0.1,
+      alignment: Alignment.center,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          Row(
-            children: <Widget>[
-              Expanded(
-                child: Text("Log in to access your networking profile and customize the app for your event experience"),
-              )
-            ],
+          IconButton(
+            icon: Icon(Icons.face),
+            onPressed: (){},
+            iconSize: MediaQuery.of(context).size.width*0.18,
           ),
-          SizedBox(height: 5,),
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: <Widget>[
-              Expanded(
-                child: IconButton(
-                  icon: Icon(Icons.face),
-                  onPressed: (){},
-                ),
-              ),
-              Expanded(
-                child: IconButton(
-                  icon: Icon(Icons.fingerprint),
-                  onPressed: (){},
-                ),
-              )
-            ],
-          )
+          IconButton(
+            icon: Icon(Icons.fingerprint),
+            onPressed: (){},
+            iconSize: MediaQuery.of(context).size.width*0.18,
+          ),
         ],
       ),
+    );
+  }
+
+  Widget _botonCorreoElectronico(){
+    return Container(
+      height: MediaQuery.of(context).size.height*0.09,
+      alignment: Alignment.center,
+      color: Colors.black12,
+      child: TextFormField(
+        keyboardType: TextInputType.emailAddress,
+        decoration: const InputDecoration(
+          border: InputBorder.none,
+          icon: Icon(Icons.person_outline),
+          hintText: 'Example: felaban@feleaban.com',
+          labelText: 'Username (your email)',
+        ),
+        onSaved: (String value) {
+          
+        },
+        validator: (String value) {
+          if(value.isEmpty)
+            return "Ingrese un valor";
+          return null;
+        },
+      ),
+    );
+  }
+
+  Widget _botonContrasena(){
+    return Container(
+      height: MediaQuery.of(context).size.height*0.09,
+      alignment: Alignment.center,
+      color: Colors.black12,
+      child: TextFormField(
+        
+        decoration: const InputDecoration(
+          border: InputBorder.none,
+          icon: Icon(Icons.vpn_key),
+          labelText: 'Password',
+        ),
+        obscureText: true,
+        onSaved: (String value) {
+          
+        },
+        validator: (String value) {
+          if (value.isEmpty ||
+              !RegExp(r"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
+                  .hasMatch(value)) {
+            return 'Por favor, ingrese un email válido';
+          }
+          return null;
+        },
+      )
     );
   }
 
   Widget _usuarioContrasena(){
     return Container(
-      padding: EdgeInsets.all(MediaQuery.of(context).size.width*0.03),
+      alignment: Alignment.center,
+      height: MediaQuery.of(context).size.height*0.2,
+      padding: EdgeInsets.symmetric(vertical: MediaQuery.of(context).size.width*0.02),
       child: Form(
-      child: Column(
-        children: <Widget>[
-          TextFormField(
-            decoration: const InputDecoration(
-              icon: Icon(Icons.supervised_user_circle),
-              hintText: 'Ejemplo: felaban@feleaban.com',
-              labelText: 'Correo electrónico',
+        key: _formKey,
+        child: Column(
+          children: <Widget>[
+            Container(
+              color: Colors.black12,
+              child: TextFormField(
+                keyboardType: TextInputType.emailAddress,
+                decoration: const InputDecoration(
+                  border: InputBorder.none,
+                  icon: Icon(Icons.person_outline),
+                  hintText: 'Example: felaban@feleaban.com',
+                  labelText: 'Username (your email)',
+                ),
+                onSaved: (String value) {
+                  
+                },
+                validator: (String value) {
+                  if(value.isEmpty)
+                    return "Ingrese un valor";
+                  return null;
+                },
+              ),
             ),
-            onSaved: (String value) {
-              
-            },
-            validator: (String value) {
-              if(value.isEmpty)
-                return "Ingrese un valor";
-              return null;
-            },
-          ),
-          TextFormField(
-            
-            decoration: const InputDecoration(
-              icon: Icon(Icons.vpn_key),
-              labelText: 'Contraseña',
-            ),
-            obscureText: true,
-            onSaved: (String value) {
-              
-            },
-            validator: (String value) {
-              if (value.isEmpty ||
-                  !RegExp(r"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
-                      .hasMatch(value)) {
-                return 'Por favor, ingrese un email válido';
-              }
-              return null;
-            },
-          ),
-        ],
+            Container(
+              color: Colors.black12,
+              child: TextFormField(
+                
+                decoration: const InputDecoration(
+                  border: InputBorder.none,
+                  icon: Icon(Icons.vpn_key),
+                  labelText: 'Password',
+                ),
+                obscureText: true,
+                onSaved: (String value) {
+                  
+                },
+                validator: (String value) {
+                  if (value.isEmpty ||
+                      !RegExp(r"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
+                          .hasMatch(value)) {
+                    return 'Por favor, ingrese un email válido';
+                  }
+                  return null;
+                },
+              ),
+            )
+          ],
+        ),
       ),
-    ),
     );
   }
 
   Widget _botonNext(){
     return Container(
+      height: MediaQuery.of(context).size.height*0.1,
       child: RaisedButton(
         child: Text("Next", style: TextStyle(color: Colors.white),),
         onPressed: (){
 
         },
         color: Colors.blue,
+        elevation: 0,
       ),
     );
   }
@@ -133,18 +240,18 @@ class _HomePageState extends State<HomePage> {
   Widget _nuevoUsuarioOlvidoPass(){
     return Row(
       children: <Widget>[
-       Expanded(
-         child:  FlatButton(
+      Expanded(
+        child:  FlatButton(
           child: Text("New user?",style: TextStyle(color: Colors.grey),),
           onPressed: (){},
         ),
-       ),
-       Expanded(
-         child:  FlatButton(
+      ),
+      Expanded(
+        child:  FlatButton(
           child: Text("Forgot password?",style: TextStyle(color: Colors.grey),),
           onPressed: (){},
         ),
-       ),
+      ),
       ],
     );
   }
@@ -154,6 +261,7 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: _barraSuperior(),
       body: _cuerpoDeLaVista(),
+      bottomNavigationBar: _nuevoUsuarioOlvidoPass(),
     );
   }
 }
